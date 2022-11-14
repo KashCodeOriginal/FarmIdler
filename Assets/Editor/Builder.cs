fe4412488643eee08165cd751c2770c72430f268
Kashfornoriginal <Kashfornoriginal@mail.ru>
2022-11-14 16:48:02 +0300

    Update-Git-Ignore

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -13,7 +13,7 @@ namespace Editor
             var report = BuildPlayer(
                 new BuildPlayerOptions {
             target = BuildTarget.Android,
-            locationPathName = "C:/Users/Lindf/Documents/GitHub/TestTaskForLavaProject/Build/Build.apk",
+            locationPathName = "C:/Users/Lindf/Documents/GitHub/TestTaskForLavaProject/artifacts/Build.apk",
             scenes = new []{"Assets/Scenes/Bootstrap.unity"}
                 });
             
