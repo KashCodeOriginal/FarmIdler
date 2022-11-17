646f3470888d4d68902e7a03fec09fd6b0a09303
Kashfornoriginal <Kashfornoriginal@mail.ru>
2022-11-17 12:05:06 +0300

    Add-Cell-Static-Data

---
+++ src/LavaProject/Assets/Scripts/Units/Bed/BedCellStaticData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Cells/BedCell", fileName = "BedCellStaticData", order = 0)]
+public class BedCellStaticData : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private int _experience;
+    [SerializeField] private int _timeBetweenGrowingStages;
+}
